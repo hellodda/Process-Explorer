6ac187d9b2b8e35e958ededb560d4c51ed2e096b
CodeOtakuu <notbukaaa777@gmail.com>
2025-05-24 17:31:13 +0400

    Refactoring

---
+++ Process-Explorer.GUI/Helpers/MemoryUsageChart.cs
@@ -0,0 +1,99 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using Process_Explorer.GUI.Models;
+using SkiaSharp;
+using System.Collections.ObjectModel;
+
+namespace Process_Explorer.GUI.Helpers
+{
+    public class MemoryUsageChart : ObservableObject
+    {
+        private readonly SKColor _strokeColor;
+        private readonly byte _fillAlpha;
+
+        private MemorySize _oldSelectedSize;
+
+        public ObservableCollection<ISeries> ChartSeries { get; private set; }
+        public ObservableCollection<ICartesianAxis> XAxes { get; private set; }
+        public ObservableCollection<ICartesianAxis> YAxes { get; private set; }
+        public ObservableCollection<MemorySize> MemorySizes { get; } = InitializeSizes();
+
+        private ObservableCollection<double> _values;
+
+        private MemorySize _selectedMemorySize;
+        public MemorySize SelectedMemorySize
+        {
+            get => _selectedMemorySize;
+            set
+            {
+                if (SetProperty(ref _selectedMemorySize, value))
+                {
+                    if (_selectedMemorySize.Value > _oldSelectedSize.Value)
+                    {
+                        for (int i = 0; i < _values.Count; ++i)
+                        {
+                            _values[i] = 0;
+                        }
+                        InitializeAxes();
+                        InitializeSeries();
+                    }
+                
+                    _oldSelectedSize = _selectedMemorySize;
+                    OnPropertyChanged(nameof(ChartSeries));
+                    OnPropertyChanged(nameof(XAxes));
+                    OnPropertyChanged(nameof(YAxes));
+                }
+            }
+        }
+
+        public MemoryUsageChart(
+            ObservableCollection<double> values,
+            MemorySize initialSize,
+            SKColor strokeColor,
+            byte fillAlpha)
+        {
+            _values = values;
+            _strokeColor = strokeColor;
+            _fillAlpha = fillAlpha;
+
+            _oldSelectedSize = initialSize;
+            _selectedMemorySize = initialSize;
+
+            InitializeAxes();
+            InitializeSeries();
+        }
+
+        private void InitializeAxes()
+        {
+            XAxes = new ObservableCollection<ICartesianAxis> { new Axis() };
+            YAxes = new ObservableCollection<ICartesianAxis> { new Axis { MinLimit = 0 } };
+        }
+
+        private void InitializeSeries()
+        {
+            ChartSeries = new ObservableCollection<ISeries>
+            {
+                new LineSeries<double>
+                {
+                    Values = _values,
+                    Stroke = new SolidColorPaint(_strokeColor) { StrokeThickness = 2 },
+                    Fill = new SolidColorPaint(new SKColor(_strokeColor.Red, _strokeColor.Green, _strokeColor.Blue, _fillAlpha)),
+                    GeometrySize = 0,
+                    YToolTipLabelFormatter = point => $"{point.Context.DataSource} {_selectedMemorySize.Name}"
+                }
+            };
+        }
+
+        public static ObservableCollection<MemorySize> InitializeSizes()
+            => new ObservableCollection<MemorySize>
+            {
+                new MemorySize("B", 1),
+                new MemorySize("KB", 1024),
+                new MemorySize("MB", 1048576),
+                new MemorySize("GB", 1073741824)
+            };
+    }
+}
--- Process-Explorer.GUI/ViewModels/MetricsViewModel.cs
+++ Process-Explorer.GUI/ViewModels/MetricsViewModel.cs
@@ -1,158 +1,95 @@
-﻿using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Painting;
-using LiveChartsCore.Kernel.Sketches;
-using SkiaSharp;
+﻿using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Linq;
-using Process_Explorer.GUI.Models;
-using Process_Explorer.BLL.HostedServices;
 using System.Threading;
-using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using Process_Explorer.BLL.HostedServices;
+using Process_Explorer.GUI.Helpers;
+using Process_Explorer.GUI.Models;
+using SkiaSharp;
 
-namespace Process_Explorer.GUI.ViewModels;
-
-public class MetricsViewModel : INotifyPropertyChanged, IDisposable
+namespace Process_Explorer.GUI.ViewModels
 {
-    private readonly ProcessMetricsHostedService _service;
-    private Timer _timer = default!;
-
-    private readonly ObservableCollection<double> _privateBytesValues = new();
-    private readonly ObservableCollection<double> _workingSetValues = new();
-
-    public ObservableCollection<ISeries> PrivateBytesChartSeries { get; private set; } = default!;
-    public ObservableCollection<ICartesianAxis> PrivateBytesXAxes { get; private set; } = default!;
-    public ObservableCollection<ICartesianAxis> PrivateBytesYAxes { get; private set; } = default!;
-
-    public ObservableCollection<ISeries> WorkingSetChartSeries { get; private set; } = default!;
-    public ObservableCollection<ICartesianAxis> WorkingSetXAxes { get; private set; } = default!;
-    public ObservableCollection<ICartesianAxis> WorkingSetYAxes { get; private set; } = default!;
-
-    public ObservableCollection<MemorySize> MemoryVarsWorkingSet { get; } = InitializeSizes();
-    public ObservableCollection<MemorySize> MemoryVarsPrivateBytes { get; } = InitializeSizes();
-
-    private MemorySize _selectedMemVarWorkingSet;
-    public MemorySize SelectedMemVarWorkingSet
+    public partial class MetricsViewModel : ObservableObject, IDisposable
     {
-        get => _selectedMemVarWorkingSet;
-        set
+        private readonly ProcessMetricsHostedService _service;
+        private readonly Timer _timer;
+        private readonly ObservableCollection<double> _privateValues = new();
+        private readonly ObservableCollection<double> _workingValues = new();
+
+        public static ObservableCollection<MemorySize> MemorySizes { get; } = MemoryUsageChart.InitializeSizes();
+
+        [ObservableProperty]
+        private MemorySize _selectedMemVarPrivateBytes;
+
+        [ObservableProperty]
+        private MemorySize _selectedMemVarWorkingSet;
+
+        private MemoryUsageChart _privateChart;
+        private MemoryUsageChart _workingChart;
+
+        public ObservableCollection<ISeries> PrivateBytesChartSeries => _privateChart.ChartSeries;
+        public ObservableCollection<ICartesianAxis> PrivateBytesXAxes => _privateChart.XAxes;
+        public ObservableCollection<ICartesianAxis> PrivateBytesYAxes => _privateChart.YAxes;
+
+        public ObservableCollection<ISeries> WorkingSetChartSeries => _workingChart.ChartSeries;
+        public ObservableCollection<ICartesianAxis> WorkingSetXAxes => _workingChart.XAxes;
+        public ObservableCollection<ICartesianAxis> WorkingSetYAxes => _workingChart.YAxes;
+
+        public MetricsViewModel(ProcessMetricsHostedService service)
         {
-            if (_selectedMemVarWorkingSet != value)
-            {
-                _selectedMemVarWorkingSet = value;
-                OnPropertyChanged();
-                ReInitialize();
-            }
+            _service = service;
+            _selectedMemVarPrivateBytes = MemorySizes.Last();
+            _selectedMemVarWorkingSet = MemorySizes.Last();
+
+            _privateChart = new MemoryUsageChart(
+                _privateValues,
+                _selectedMemVarPrivateBytes,
+                SKColors.Red,
+                40);
+
+            _workingChart = new MemoryUsageChart(
+                _workingValues,
+                _selectedMemVarWorkingSet,
+                SKColors.Yellow,
+                40);
+
+            _timer = new Timer(UpdateMetrics, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
         }
-    }
 
-    private MemorySize _selectedMemVarPrivateBytes;
-    public MemorySize SelectedMemVarPrivateBytes
-    {
-        get => _selectedMemVarPrivateBytes;
-        set
+        private void UpdateMetrics(object? state)
         {
-            if (_selectedMemVarPrivateBytes != value)
-            {
-                _selectedMemVarPrivateBytes = value;
-                OnPropertyChanged();
-                ReInitialize();
-            }
+            var procs = _service.processes;
+            if (!procs.Any()) return;
+
+            var sumP = procs.Sum(p => p.PrivateBytes) / SelectedMemVarPrivateBytes.Value;
+            var sumW = procs.Sum(p => p.WorkingSet) / SelectedMemVarWorkingSet.Value;
+
+            _privateValues.Add(sumP);
+            _workingValues.Add(sumW);
+
+            if (_privateValues.Count > 50) _privateValues.RemoveAt(0);
+            if (_workingValues.Count > 50) _workingValues.RemoveAt(0);
         }
-    }
 
-    public MetricsViewModel(ProcessMetricsHostedService service)
-    {
-        _service = service;
-        _selectedMemVarWorkingSet = MemoryVarsWorkingSet.Last(); 
-        _selectedMemVarPrivateBytes = MemoryVarsPrivateBytes.Last();
-      
-        InitializeSeriesAndAxes();
-
-        _timer = new Timer(UpdateMetrics, service, TimeSpan.Zero, TimeSpan.FromSeconds(1));
-    }
-
-    private void InitializeSeriesAndAxes()
-    {
-        _privateBytesValues.Clear();
-        _workingSetValues.Clear();
-
-        PrivateBytesChartSeries = new ObservableCollection<ISeries>
+        partial void OnSelectedMemVarPrivateBytesChanged(MemorySize oldVal, MemorySize newVal)
         {
-            CreateSeries(_privateBytesValues, SKColors.Red, 40)
-        };
-        PrivateBytesXAxes = new() { new Axis() };
-        PrivateBytesYAxes = new() { new Axis { MinLimit = 0 } };
-
-        WorkingSetChartSeries = new ObservableCollection<ISeries>
-        {
-            CreateSeries(_workingSetValues, SKColors.Yellow, 40)
-        };
-        WorkingSetXAxes = new() { new Axis() };
-        WorkingSetYAxes = new() { new Axis { MinLimit = 0 } };
-
-        OnPropertyChanged(nameof(PrivateBytesChartSeries));
-        OnPropertyChanged(nameof(PrivateBytesXAxes));
-        OnPropertyChanged(nameof(PrivateBytesYAxes));
-        OnPropertyChanged(nameof(WorkingSetChartSeries));
-        OnPropertyChanged(nameof(WorkingSetXAxes));
-        OnPropertyChanged(nameof(WorkingSetYAxes));
-    }
-
-    private LineSeries<double> CreateSeries(
-        ObservableCollection<double> values,
-        SKColor color,
-        byte alpha)
-    {
-        return new LineSeries<double>
-        {
-            Values = values,
-            Stroke = new SolidColorPaint(color) { StrokeThickness = 2},
-            Fill = new SolidColorPaint(new SKColor(color.Red, color.Green, color.Blue, alpha)),
-            GeometrySize = 0
-        };
-    }
-
-    static private ObservableCollection<MemorySize> InitializeSizes()
-        => new ObservableCollection<MemorySize>
-        {
-            new MemorySize("B", 1),
-            new MemorySize("KB", 1024),
-            new MemorySize("MB", 1048576),
-            new MemorySize("GB", 1073741824)
-        };
-
-    private void ReInitialize()
-    {
-        InitializeSeriesAndAxes();
-    }
-
-    private void UpdateMetrics(object? state)
-    {
-        var processes = _service.processes;
-
-        if (processes.Any())
-        {
-            var sumPrivate = processes.Sum(p => p.PrivateBytes) / SelectedMemVarPrivateBytes.Value;
-            var sumWorking = processes.Sum(p => p.WorkingSet) / SelectedMemVarWorkingSet.Value;
-
-            _privateBytesValues.Add(sumPrivate);
-            _workingSetValues.Add(sumWorking);
-
-            if (_privateBytesValues.Count > 50) _privateBytesValues.RemoveAt(0);
-            if (_workingSetValues.Count > 50) _workingSetValues.RemoveAt(0);
+            _privateChart.SelectedMemorySize = newVal;
+            OnPropertyChanged(nameof(PrivateBytesChartSeries));
+            OnPropertyChanged(nameof(PrivateBytesXAxes));
+            OnPropertyChanged(nameof(PrivateBytesYAxes));
         }
-    }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+        partial void OnSelectedMemVarWorkingSetChanged(MemorySize oldVal, MemorySize newVal)
+        {
+            _workingChart.SelectedMemorySize = newVal;
+            OnPropertyChanged(nameof(WorkingSetChartSeries));
+            OnPropertyChanged(nameof(WorkingSetXAxes));
+            OnPropertyChanged(nameof(WorkingSetYAxes));
+        }
 
-    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    public void Dispose()
-    {
-        _timer?.Dispose();
+        public void Dispose() => _timer.Dispose();
     }
 }
